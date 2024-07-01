386329c2da4dffeb91a5191504a3781d6f68d071
Димитрија Тимески <32930367+DimitrijaT@users.noreply.github.com>
2024-07-01 22:13:25 +0200

    added label with text and image

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
         // JFrame = a GUI window to add components to
@@ -9,26 +13,56 @@ public class Main {
         The other alternative is to create a class that extends JFrame and then create an object of that class
          */
 
-        /*
         JFrame frame = new JFrame("Hello world!");
-        frame.setTitle("JFrame Title Rewritten"); // sets the frame title
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // closes the frame when the close button is clicked
-
-        frame.setResizable(false); // makes the frame non-resizable
-        frame.setSize(300, 300); // sets the x-dimension and y-dimension of the frame
-
-        frame.setVisible(true); // makes the frame visible
+        frame.setTitle("JFrame Title Rewritten"); // changes the frame title
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setResizable(false); // makes the frame non-resizable
+        frame.setSize(1000, 1000);
 
         ImageIcon image = new ImageIcon("src/main/resources/logo.png");
         frame.setIconImage(image.getImage()); // change the icon of the frame
-        frame.getContentPane().setBackground(new Color(123, 50, 250)); // White
-        */
+        frame.getContentPane().setBackground(new Color(123, 50, 250));
+        // == Set layout manager ==
+//        frame.setLayout(null); // this means we can use absolute positioning
 
         // == Using a class that extends JFrame ==
 //        MyFrame myFrame = new MyFrame();
-        new MyFrame();
+//        new MyFrame();
 
+        Border border = BorderFactory.createLineBorder(Color.GREEN, 3);
 
+        // JLabel = a GUI display area for a string of text, an image, or both
+        // == Create a Label ==
+        JLabel label = getLabel(border);
+        frame.add(label);
+
+        frame.setVisible(true); // makes the frame visible
+        frame.pack(); // resizes the frame so all components are at their preferred sizes
+    }
+
+    private static JLabel getLabel(Border border) {
+        JLabel label = new JLabel();
+        label.setText("Java Swing GUI Application");
+
+        ImageIcon labelImage = new ImageIcon("src/main/resources/kiimo.png");
+        label.setIcon(labelImage);
+        label.setHorizontalTextPosition(JLabel.CENTER); // sets text LEFT, CENTER, RIGHT
+        label.setVerticalTextPosition(JLabel.TOP); // sets text TOP, CENTER, BOTTOM
+        label.setForeground(new Color(0x00FF00));
+        label.setFont(new Font("MV Boli", Font.PLAIN, 20));
+        label.setIconTextGap(100);
+
+        // These 2 go together
+        label.setBackground(Color.BLACK);
+        label.setOpaque(true);
+
+        label.setBorder(border);
+        label.setVerticalAlignment(JLabel.CENTER); // sets vertical position of icon+text within label
+        label.setHorizontalAlignment(JLabel.CENTER); // sets horizontal position of icon+text within label
+
+        // == Set Bounds, works only if layout is set to null ==
+//        label.setBounds(100, 100, 500, 500); // sets x, y, width, height of label
+        return label;
     }
 }
 
--- src/main/java/org/example/MyFrame.java
+++ src/main/java/org/example/MyFrame.java
@@ -8,22 +8,26 @@ public class MyFrame extends JFrame {
     MyFrame() {
 
         this.setTitle("JFrame Title Rewritten"); // sets title of frame
+
         // == Close Operation ==
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // closes the frame when the close button is clicked
+
         // == Set Size ==
         this.setResizable(false); // makes the frame non-resizable
-        this.setSize(300, 300); // sets the x-dimension and y-dimension of the frame
+        this.setSize(300, 300);
+
         // == Set Layout ==
         this.setVisible(true); // makes the frame visible
+
         // == Change Icon ==
         ImageIcon image = new ImageIcon("src/main/resources/logo.png");
         this.setIconImage(image.getImage()); // change the icon of the frame
+
         // == Change Background Color ==
-//        frame.getContentPane().setBackground(Color.green);
-//        frame.getContentPane().setBackground(new Color(0, 0, 0)); // Black
-//        frame.getContentPane().setBackground(new Color(255, 255, 255)); // White
-//        frame.getContentPane().setBackground(new Color(0xFFFFFF)); // White
-        this.getContentPane().setBackground(new Color(123, 50, 250)); // White
+        // frame.getContentPane().setBackground(Color.green);
+        // frame.getContentPane().setBackground(new Color(0, 0, 0)); // Black
+        // frame.getContentPane().setBackground(new Color(0xFFFFFF)); // White
+        this.getContentPane().setBackground(new Color(123, 50, 250));
 
 
     }
